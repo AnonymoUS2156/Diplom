372b3eb2a597a5095eba7d5d87458e4229507f0f
gigra <gigra@DESKTOP-V9EOS73>
2024-06-07 12:18:44 +0300

    добавлены модели и тп

--- Forms/AuthForm.Designer.cs
+++ Forms/AuthForm.Designer.cs
@@ -28,15 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.buttonEnter = new MaterialSkin.Controls.MaterialRaisedButton();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.authArchivistBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.authArchivistBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonEnter
             // 
             this.buttonEnter.Depth = 0;
-            this.buttonEnter.Location = new System.Drawing.Point(318, 306);
+            this.buttonEnter.Location = new System.Drawing.Point(312, 376);
             this.buttonEnter.MouseState = MaterialSkin.MouseState.HOVER;
             this.buttonEnter.Name = "buttonEnter";
             this.buttonEnter.Primary = true;
@@ -49,23 +56,75 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Diploma.Properties.Resources.w0n7cislc3lpalnnhz6yo1qgq1baqbr3;
-            this.pictureBox1.Location = new System.Drawing.Point(258, 67);
+            this.pictureBox1.Location = new System.Drawing.Point(312, 63);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(298, 217);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.Size = new System.Drawing.Size(165, 137);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            // 
+            // authArchivistBindingSource
+            // 
+            this.authArchivistBindingSource.DataSource = typeof(Diploma.Models.AuthArchivist);
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.authArchivistBindingSource, "Login", true));
+            this.loginTextBox.Location = new System.Drawing.Point(264, 219);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(245, 20);
+            this.loginTextBox.TabIndex = 4;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.authArchivistBindingSource, "Password", true));
+            this.passwordTextBox.Location = new System.Drawing.Point(264, 281);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(245, 20);
+            this.passwordTextBox.TabIndex = 6;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(205, 220);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(53, 19);
+            this.materialLabel1.TabIndex = 7;
+            this.materialLabel1.Text = "Логин";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(196, 282);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(62, 19);
+            this.materialLabel2.TabIndex = 8;
+            this.materialLabel2.Text = "Пароль";
             // 
             // AuthForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 458);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonEnter);
             this.Name = "AuthForm";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.authArchivistBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,6 +132,11 @@
 
         private MaterialSkin.Controls.MaterialRaisedButton buttonEnter;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.BindingSource authArchivistBindingSource;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
     }
 }
 
--- Forms/AuthForm.cs
+++ Forms/AuthForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Diploma.Models;
 using MaterialSkin;
 using MaterialSkin.Controls;
 namespace Diploma
@@ -21,14 +22,20 @@ namespace Diploma
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
             materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue800, Primary.Blue900, Primary.Blue400, Accent.LightBlue100, TextShade.WHITE);
         }
-
-            //materalSkinMan = MaterialSkinManager.Instance;
-
+        
 
         
 
         private void buttonEnter_Click_1(object sender, EventArgs e)
         {
+            Model1 model1 = new Model1();
+            if (String.IsNullOrWhiteSpace(loginTextBox.Text)
+                || String.IsNullOrWhiteSpace(passwordTextBox.Text))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+
             Hide();
             MainForm mainForm = new MainForm();
             mainForm.ShowDialog();
